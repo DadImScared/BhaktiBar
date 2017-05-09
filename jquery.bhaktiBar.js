5ffee30f696806384f3ba4622cc10305df487e67
Matthew Bernheimer <Krutonslol@gmail.com>
2017-05-09 19:13:38 -0400

    fix snippets

--- jquery.bhaktiBar.js
+++ jquery.bhaktiBar.js
@@ -235,7 +235,7 @@
       html += '</p>';
       html += '<h4>Content</h4>';
       html += '<div class="snippets">';
-      item.content.forEach(function(snippet) {
+      item.displayContent.forEach(function(snippet) {
         html += '<div class="snippet">';
         html += snippet.replace(regex, '<span class="color-book-results">' + query + '</span>');
         html += '</div>';
